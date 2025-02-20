329bde6502b80a60fa875597e9e8c533a19caa0f
kunj1610 <U2404035@UEL.AC.UK>
2025-02-20 18:03:59 +0000

    intial

--- src/controllers/weatherController.ts
+++ src/controllers/weatherController.ts
@@ -2,6 +2,9 @@ import { Request, Response } from "express";
 import {
   generateDublinWeatherData,
   generateLondonWeatherData,
+  generateSpainWeatherData,
+  generateGreeceWeatherData,
+  ForecastLondonData,
 } from "../services/weatherService.js";
 import { validationResult } from "express-validator";
 
@@ -10,6 +13,42 @@ import { validationResult } from "express-validator";
  * @param req the request object
  * @param res the response object
  */
+export const getForecastLondonData = async (req: Request, res: Response) => {
+  // We will use the validationResult function to check if there are any validation errors
+  const errors = validationResult(req);
+
+  // If there are validation errors, we will log them and send a 400 status code
+  if (!errors.isEmpty()) {
+    console.error("Validation error", errors.mapped());
+    res.status(400).json({ errors: errors.array() });
+    return;
+  }
+
+  try {
+    // Get the city param from the request
+    const { city } = req.params;
+    console.log(city);
+
+    // We will create a variable with a type of ForecastData
+    let finalForecastData: ForecastData;
+
+    // We will use an if statement to check which city was passed in
+    if (city === "london") {
+      console.log(ForecastLondonData());
+      finalForecastData = ForecastLondonData();
+    } else {
+      // If the city is not london, we will throw an error
+      res.status(404).send("City not found");
+    }
+    res.status(200).json(finalForecastData);
+
+    // We will return the weather data as JSON
+  } catch (error) {
+    // If there is an error, we will log it and send a 500 status code
+    res.status(500).send("Error in fetching weather data");
+  }
+}
+
 export const getWeatherData = async (req: Request, res: Response) => {
   // We will use the validationResult function to check if there are any validation errors
   const errors = validationResult(req);
@@ -36,6 +75,10 @@ export const getWeatherData = async (req: Request, res: Response) => {
       finalWeatherData = generateLondonWeatherData();
     } else if (city === "dublin") {
       finalWeatherData = generateDublinWeatherData();
+    } else if (city === "spain") {
+      finalWeatherData = generateSpainWeatherData();
+    } else if (city === "greece") {
+      finalWeatherData = generateGreeceWeatherData();
     } else {
       // If the city is not london or dublin, we will throw an error
       res.status(404).send("City not found");
--- src/services/weatherService.ts
+++ src/services/weatherService.ts
@@ -13,12 +13,50 @@ export const generateLondonWeatherData = (): WeatherData => {
   return generatedWeatherData;
 };
 
+export const ForecastLondonData = (): ForecastData => {
+  //Forecast London three-day temperature
+  const ForecastData = {
+    dayOne: 12,
+    dayTwo: 23,
+    dayThree: 17
+  };
+
+  //return forecast
+  return ForecastData;
+}
+
 export const generateDublinWeatherData = (): WeatherData => {
   // Generate random weather data
   const generatedWeatherData: WeatherData = {
     temperature: faker.number.int({ min: -15, max: 30 }),
     humidity: faker.number.int({ min: 79, max: 86 }),
     wind: faker.number.int({ min: 2, max: 78 }),
+    rain: faker.number.int({ min: 65, max: 75 }),
+  };
+
+  // Return weather data
+  return generatedWeatherData;
+};
+
+export const generateSpainWeatherData = (): WeatherData => {
+  // Generate random weather data
+  const generatedWeatherData: WeatherData = {
+    temperature: faker.number.int({ min: -15, max: 35 }),
+    humidity: faker.number.int({ min: 60, max: 80 }),
+    wind: faker.number.int({ min: 2, max: 78 }),
+    rain: faker.number.int({ min: 65, max: 75 }),
+  };
+
+  // Return weather data
+  return generatedWeatherData;
+};
+
+export const generateGreeceWeatherData = (): WeatherData => {
+  // Generate random weather data
+  const generatedWeatherData: WeatherData = {
+    temperature: faker.number.int({ min: -15, max: 35 }),
+    humidity: faker.number.int({ min: 50, max: 80 }),
+    wind: faker.number.int({ min: 4, max: 78 }),
     rain: faker.number.int({ min: 65, max: 75 }),
   };
 
